97ca1448ca3370ce0a7c0c03f62fc7cad5ea5c5e
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-23 14:06:21 -0500

    Oculta o muestra una frase

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,4 +4,12 @@ import {Component} from '@angular/core';
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
-export class BodyComponent {}
+export class BodyComponent {
+    
+    mostrar = true;
+    
+    frase: any = {
+        mensaje: "Un gran poder conlleva una gran responsabilidad",
+        autor: "Peter Parker"
+    }
+}
